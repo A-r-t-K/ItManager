9d8950071cf1eb4d622ca9c93ae7a7039105e670
John Heavy <boss.vanya20@mail.ru>
2021-09-07 11:34:51 +0300

    Initial commit

---
+++ src/main/java/by/sachishin/demo/SpringitmanagerApplication.java
@@ -0,0 +1,13 @@
+package by.sachishin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringitmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringitmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/sachishin/demo/SpringitmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.sachishin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
